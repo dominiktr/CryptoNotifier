07ccac1688299a937f01b3ab926d34c3dd699717
dominiktr <deetee1011@gmail.com>
2023-07-01 15:17:40 +0200

    Alarm creation functionality added

--- lib/bloc/edit_alarm/edit_alarm_view.dart
+++ lib/bloc/edit_alarm/edit_alarm_view.dart
@@ -1,7 +1,9 @@
 import 'package:cryptonotifier/bloc/edit_alarm/edit_alarm_bloc.dart';
 import 'package:cryptonotifier/bloc/edit_alarm/edit_alarm_state.dart';
 import 'package:cryptonotifier/bloc/navigation/nav_cubit.dart';
+import 'package:cryptonotifier/models/alarm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
@@ -28,11 +30,14 @@ class _AlarmViewStateful extends StatefulWidget {
 }
 
 class _AlarmViewState extends State<_AlarmViewStateful> {
+  List<Alarm> alarms = [];
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<EditAlarmBloc, AlarmState>(
       builder: (context, state) {
         if (state is AlarmEditView) {
+          alarms = state.crypto.alarms;
           return Scaffold(
             backgroundColor: _colorScheme.bg1,
             body: Padding(
@@ -90,7 +95,7 @@ class _AlarmViewState extends State<_AlarmViewStateful> {
                     Expanded(
                       child: AnimationLimiter(
                         child: ListView.builder(
-                          itemCount: state.crypto.alarms.length,
+                          itemCount: alarms.length,
                           itemBuilder: (context, index) =>
                               AnimationConfiguration.staggeredList(
                             position: index,
@@ -108,14 +113,14 @@ class _AlarmViewState extends State<_AlarmViewStateful> {
                                         Row(
                                           children: [
                                             Icon(
-                                              state.crypto.alarms[index].high
+                                              alarms[index].high
                                                   ? Icons.arrow_upward
                                                   : Icons.arrow_downward,
                                               color: _colorScheme.font,
                                             ),
                                             const SizedBox(width: 10),
                                             Text(
-                                              "${state.crypto.alarms[index].price_target}\$",
+                                              "${alarms[index].price_target}\$",
                                               style: TextStyle(
                                                 color: _colorScheme.font,
                                               ),
@@ -123,17 +128,21 @@ class _AlarmViewState extends State<_AlarmViewStateful> {
                                           ],
                                         ),
                                         Switch(
-                                          value:
-                                              state.crypto.alarms[index].active,
+                                          value: alarms[index].active,
                                           onChanged: (value) {
                                             setState(() {
-                                              state.crypto.alarms[index]
-                                                  .active = value;
+                                              alarms[index].active = value;
                                             });
                                           },
                                         )
                                       ],
                                     ),
+                                    onTap: () => _expandCard(
+                                        context,
+                                        index,
+                                        alarms[index].price_target,
+                                        alarms[index].high,
+                                        alarms[index].active),
                                   ),
                                 ),
                               ),
@@ -147,7 +156,9 @@ class _AlarmViewState extends State<_AlarmViewStateful> {
               ),
             ),
             floatingActionButton: FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                _addCard(state.crypto.symbol);
+              },
               child: const Icon(Icons.add),
             ),
             floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
@@ -158,4 +169,194 @@ class _AlarmViewState extends State<_AlarmViewStateful> {
       },
     );
   }
+
+  void _addCard(String symbol) {
+    setState(() {
+      alarms.add(Alarm(
+          crypto_symbol: symbol, price_target: 0, high: true, active: false));
+    });
+    _expandCard(context, alarms.length - 1, 0, true, false);
+  }
+
+  final MaterialStateProperty<Icon?> thumbIcon =
+      MaterialStateProperty.resolveWith<Icon?>(
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Icon(Icons.check);
+      }
+      return const Icon(Icons.close);
+    },
+  );
+
+  void _expandCard(BuildContext context, int index, double target_price,
+      bool high, bool active) {
+    String price = target_price.toString();
+    TextEditingController _textFieldController =
+        TextEditingController(text: price);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              backgroundColor: _colorScheme.bg2,
+              content: SizedBox(
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "The price that triggers the notification:",
+                      style: TextStyle(
+                        color: _colorScheme.font,
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        style: TextStyle(
+                          color: _colorScheme.font,
+                        ),
+                        decoration: const InputDecoration(
+                          hintText: "0.00",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueAccent),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueAccent),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.allow(
+                              RegExp(r'^\d*\.?\d*$')),
+                        ],
+                        controller: _textFieldController,
+                        onChanged: (value) => price = value,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'High/Low: ',
+                          style: TextStyle(
+                            color: _colorScheme.font,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Switch(
+                              value: high,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  high = value;
+                                });
+                              },
+                            ),
+                            GestureDetector(
+                              child: Icon(
+                                high
+                                    ? Icons.arrow_upward
+                                    : Icons.arrow_downward,
+                                color: _colorScheme.font,
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  high = !high;
+                                });
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Active: ',
+                          style: TextStyle(
+                            color: _colorScheme.font,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Switch(
+                              value: active,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  active = value;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 24,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text(
+                    'Save',
+                    style: TextStyle(
+                      color: _colorScheme.font,
+                    ),
+                  ),
+                  onPressed: () {
+                    alarms[index].price_target = double.parse(price);
+                    alarms[index].high = high;
+                    alarms[index].active = active;
+                    saveToSharedPreferences();
+                    refreshView();
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text(
+                    'Delete',
+                    style: TextStyle(
+                      color: _colorScheme.font,
+                    ),
+                  ),
+                  onPressed: () {
+                    alarms.removeAt(index);
+                    saveToSharedPreferences();
+                    refreshView();
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text(
+                    'Close',
+                    style: TextStyle(
+                      color: _colorScheme.font,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    ).then((value) {
+      refreshView();
+    });
+  }
+
+  void refreshView() {
+    setState(() {});
+  }
+
+  void saveToSharedPreferences() {}
 }
